30a297b60ce57aa1121d11eb45bd64098d68d401
Akond Rahman <akond.rahman.buet@gmail.com>
2018-09-14 15:53:40 -0400

    add bug miner

--- bug_miner.py
+++ bug_miner.py
@@ -7,6 +7,30 @@ import pandas as pd
 import numpy as np
 import json 
 import requests 
+def processDict(dict_p):
+    data_list = []
+    if 'bugs' in dict_p:
+       bug_data = dict_p['bugs']
+       for bug_ in bug_data:
+           
+           alias          = bug_['alias']
+           involved_count = len(bug_['cc']) + 1 # cc people + assigned 
+           comment_count  = bug_['comment_count']
+           component      = bug_['component']           
+           create_ts      = bug_['creation_time']
+
+           priority       = bug_['priority']
+           product        = bug_['product']           
+           resolution     = bug_['resolution']           
+
+           severity       = bug_['severity']           
+           status         = bug_['status']           
+
+           data_list = [alias, involved_count, comment_count, component, create_ts, priority, product, resolution, severity, status]
+    
+    return data_list
+
+
 
 def getBugData(df_param):
     bugIDs = np.unique( df_param['BUG_ID'].tolist() )
@@ -14,7 +38,9 @@ def getBugData(df_param):
         req_url  = 'https://bugzilla.mozilla.org/rest/bug/' + str(bugID).strip()    
         bug_data = requests.get(req_url)           
         bug_dict = bug_data.json()
-        print bug_dict 
+        # print bug_dict 
+        data_for_bug = processDict(bug_dict)
+        print bugID, data_for_bug
         print '*'*50
 
 
