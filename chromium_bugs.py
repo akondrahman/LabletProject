15b90469647722301f827a74d459d977616ed53d
Akond Rahman <akond.rahman.buet@gmail.com>
2019-07-04 14:25:14 -0400

    Mine chromium bug data

--- chromium_bugs.py
+++ chromium_bugs.py
@@ -29,7 +29,7 @@ def getJSONData(file_list):
      u'state', u'etag', u'closed', u'status', u'updated', u'stars', u'canComment', u'status_modified', 
      u'kind', u'author', u'summary', u'components', u'published', u'title', u'starred', u'canEdit']
     '''
-    cve_cnt = 0 
+    cve_cnt , cve_comment_cnt , cve_comment_compo_cnt = 0, 0, 0 
     for file_ in file_list:
         if os.path.exists(file_):
             with open(file_, 'rU') as json_file: 
@@ -40,30 +40,39 @@ def getJSONData(file_list):
                         bug_labels = dict_['labels']
                         for bug_label in bug_labels:
                             if 'cve' in bug_label.lower():
-                                if (('id' in dict_) and ('summary' in dict_) and ('comments' in dict_)):
-                                    bug_comments = dict_['comments']
+                                if (('id' in dict_) and ('summary' in dict_) and ('comments' in dict_) and ('components' in dict_)):
+                                    bug_comments   = dict_['comments']
+                                    bug_components = dict_['components']
                                     '''
                                     keys in comment dict: 
                                     [u'kind', u'canDelete', u'author', u'is_description', u'content', u'updates', u'published', u'id']
                                     '''
                                     if len(bug_comments) > 0 :
+                                        cve_comment_cnt += 1
                                         for comment_ in bug_comments:
-                                            # print comment_.keys()
-                                            commentContent, commentDate, commentID, commentAuthor = comment_['content'] , comment_['published'], comment_['id'], comment_['author']['htmlLink']
-                                            bugReportTitle, bugReportSummary, bugReportDate, bugReportID, bugReportAuthor = dict_['title'] , dict_['summary'] , dict_['published'], dict_['id'], dict_['author']['htmlLink']
-                                            print 'Comment text:', commentContent
-                                            print 'Comment date:', commentDate
-                                            print 'Bug report title:', bugReportTitle
-                                            print 'Bug report summary:', bugReportSummary
-                                            print 'Bug report date:', bugReportDate
-                                            print 'Bug report label:', bug_label 
-                                            print 'Bug report ID:', bugReportID
-                                            print '*'*10            
-                                            full_data_list.append( (bugReportID, bugReportDate, bugReportTitle, bugReportSummary, bug_label, bugReportAuthor, commentID, commentContent, commentDate, commentAuthor)  )
+                                            if len(bug_components) > 0:
+                                                cve_comment_compo_cnt += 1 
+                                                for bug_compo in bug_components:
+                                                    # print comment_.keys()
+                                                    commentContent, commentDate, commentID, commentAuthor = comment_['content'] , comment_['published'], comment_['id'], comment_['author']['htmlLink']
+                                                    bugReportTitle, bugReportSummary, bugReportDate, bugReportID, bugReportAuthor = dict_['title'] , dict_['summary'] , dict_['published'], dict_['id'], dict_['author']['htmlLink']
+                                                    print 'Comment text:', commentContent
+                                                    print 'Comment date:', commentDate
+                                                    print 'Bug report title:', bugReportTitle
+                                                    print 'Bug report summary:', bugReportSummary
+                                                    print 'Bug report date:', bugReportDate
+                                                    print 'Bug report label:', bug_label 
+                                                    print 'Bug report component:', bug_compo
+                                                    print 'Bug report ID:', bugReportID
+                                                    print '*'*10            
+                                                    full_data_list.append( (bugReportID, bugReportDate, bugReportTitle, bugReportSummary, bug_label, bugReportAuthor, commentID, commentContent, commentDate, commentAuthor, bug_compo)  )
 
                                     cve_cnt += 1
     print '='*50 
-    print 'Bug reports with CVEs:', cve_cnt
+    print 'Total bug reports:', len(dict_list) 
+    # print 'Bug reports with CVEs:', cve_cnt
+    # print 'Bug reports with CVEs and non-zero comment:', cve_comment_cnt
+    # print 'Bug reports with CVEs and non-zero comment and non zero components:', cve_comment_compo_cnt
     return full_data_list
 
 def giveTimeStamp():
@@ -83,8 +92,8 @@ if __name__=='__main__':
     all_json_files = findAllJSONFiles(bug_repor_dir)
     # print all_json_files
     full_list = getJSONData(all_json_files)
-    full_df   = pd.DataFrame(full_list, header=['BUG_ID', 'BUG_DATE', 'BUG_TITLE', 'BUG_SUMMARY', 'BUG_CVE', 'BUG_AUTHOR', 'COMMENT_ID', 'COMMENT_TEXT', 'COMMENT_DATE', 'COMMENT_AUTHOR'])
-    full_df.to_csv(dataset_csv_name,  index=False)
+    full_df   = pd.DataFrame(full_list)
+    full_df.to_csv(dataset_csv_name, header=['BUG_ID', 'BUG_DATE', 'BUG_TITLE', 'BUG_SUMMARY', 'BUG_CVE', 'BUG_AUTHOR', 'COMMENT_ID', 'COMMENT_TEXT', 'COMMENT_DATE', 'COMMENT_AUTHOR', 'BUG_COMPO'], index=False)
     pickle.dump(full_df, open(pickle_csv_name, 'wb'))       
 
     print '*'*100
