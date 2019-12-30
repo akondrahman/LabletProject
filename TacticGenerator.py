4eb1aef08050d429b3af04353bd2bbba7c421a57
Akond Rahman <akond.rahman.buet@gmail.com>
2019-12-29 22:49:06 -0600

    prepare tactic dataframes for redhat

--- TacticGenerator.py
+++ TacticGenerator.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np 
 import math
 import datetime
+import _pickle as pickle 
 
 '''
 For Openstack 
@@ -42,7 +43,9 @@ def renameStrategy(name_):
              'PaylaodInjection':'PAYLOAD' , 
              'SourcecodeWeakness':'DIAGNOSTICS', 
              'OutdatedDependency':'DIAGNOSTICS', 
-             'SourceCodeExploration':'DIAGNOSTICS'
+             'SourceCodeExploration':'DIAGNOSTICS', 
+             'Fuzzer':'PAYLOAD', 
+             'Sourcecode':'DIAGNOSTICS'
             }
     return dict_[name_] 
     
@@ -91,6 +94,51 @@ def getOpenstackBugTime(bugID):
     the_bug      = launchpad.bugs[bugID]  
     bug_date     = the_bug.date_created
     return bug_date 
+
+
+def getBugDetails(dic, ID): 
+    cve, date_ = 'NOT_FOUND', 'NOT_FOUND' 
+    if ID in dic: 
+        cve_option_1 = dic[ID][0][0].lower()
+        cve_option_2 = dic[ID][9].split(' ')[0].lower() 
+        if 'cve' in cve_option_1:
+            cve = cve_option_1
+        elif 'cve' in cve_option_2:
+            cve = cve_option_2
+        date_ = dic[ID][3]
+    return cve, date_ 
+
+def finalizeRedHatDataFrame(df_param, pkl_ ): 
+    BUG_ID_LIST = np.unique(df_param['BUGID'].tolist()) 
+    BUG_ID_LIST = [int(x_) for x_ in BUG_ID_LIST if ( math.isnan(float(x_)) == False ) ]
+    complete_list = []
+    WHOLE_BUG_DICT = pkl_
+    for bugID in BUG_ID_LIST:
+        final_strategy_ls = []
+        # print(bugID)
+        cve , date_time = getBugDetails(WHOLE_BUG_DICT, bugID )  
+        old_strategy = df_param[df_param['BUGID']==bugID]['STRATEGY'].tolist()[0]
+        if isinstance(old_strategy, str): 
+               if 'SourceCodeExploration/SourceCodeWeakness' in old_strategy:
+                   final_strategy_ls.append( renameStrategy(old_strategy) )
+        
+        new_strategy = df_param[df_param['BUGID']==bugID]['AKOND'].tolist()[0]
+        # print(new_strategy )
+        if isinstance(new_strategy, str): 
+            if '+' in new_strategy:
+                new_strategy_ls = new_strategy.split('+') 
+                # print(new_strategy_ls)
+                for name in new_strategy_ls:
+                    if ' ' not in name:
+                        final_strategy_ls.append(  renameStrategy(name) )
+            else:
+                if ' ' not in new_strategy:
+                    final_strategy_ls.append(  renameStrategy(new_strategy)  )
+        # print(bugID, cve, final_strategy_ls) 
+        for tactic in final_strategy_ls:
+            complete_list.append((bugID, date_time, cve, tactic))
+    final_df = pd.DataFrame(complete_list)
+    return final_df
 
 def finalizeOpenstackDataFrame(df_param ):
     BUGLINKS = df_param['Link'].tolist()
@@ -166,10 +214,18 @@ if __name__=='__main__':
     # final_df =finalizeDataFrame(DS_FRAME)
 
 
-    #### GENTOO
-    DS_NAME='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-GENTOO-MAPPING-SEMIFINAL.csv'
-    OUT_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-GENTOO-MAPPING-FINAL.csv'
+    # #### GENTOO
+    # DS_NAME='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-GENTOO-MAPPING-SEMIFINAL.csv'
+    # OUT_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-GENTOO-MAPPING-FINAL.csv'
+    # DS_FRAME = pd.read_csv(DS_NAME) 
+    # final_df =finalizeDataFrame(DS_FRAME)
+
+    #### REDHAT
+    DS_NAME  ='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-REDHAT-MAPPING-SEMIFINAL.csv'
+    PKL_FILE ='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/RAW/FINAL_REDHAT_BUG_DETAILS.PKL'
+    OUT_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-REDHAT-MAPPING-FINAL.csv'
     DS_FRAME = pd.read_csv(DS_NAME) 
-    final_df =finalizeDataFrame(DS_FRAME)
+    PKL_DAT  = pickle.load(open(PKL_FILE, 'rb'), encoding="latin1")
+    final_df =finalizeRedHatDataFrame(DS_FRAME, PKL_DAT)
 
     final_df.to_csv(OUT_FILE, header=['BUGID', 'TIMESTAMP', 'CVE', 'TACTIC' ], index=False, encoding='utf-8')
