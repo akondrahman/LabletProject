cc84dfff6191d574459d29ec5e6e5696bd71da9c
Akond Rahman <akond.rahman.buet@gmail.com>
2019-12-30 17:38:36 -0600

    create final dataframe

--- ResultDataGenerator.py
+++ ResultDataGenerator.py
@@ -6,12 +6,24 @@ Script to merge mapping data and nvd cve data
 '''
 import pandas as pd 
 import numpy as np 
+from collections import Counter 
 
 def mergeDataFrame(nvd_df, mapping_df):
-    mapping_df =  mapping_df['CVE'] = mapping_df['CVE'].str.upper() 
+    mapping_df['CVE'] =  mapping_df['CVE'].str.upper() 
+    # print(mapping_df.head())
     merged_df  =  nvd_df.merge(mapping_df, left_on='ID', right_on='CVE')
     return merged_df 
 
+def printMappedCWEs(full_loaded_df):
+    tactics = np.unique(full_loaded_df['TACTIC'].tolist())
+    for tactic in tactics:
+        tactic_df   = full_loaded_df[full_loaded_df['TACTIC']==tactic]
+        tactic_cwes = tactic_df['CWE'].tolist() 
+        tactic_cwes = [x_ for x_ in tactic_cwes if x_!='NVD-CWE-Other' ]
+        tactic_cwe_dist = dict(Counter(tactic_cwes)) 
+        for k_, v_ in tactic_cwe_dist.items():
+            print('TACTIC:{}, CWE:{}, COUNT:{}', tactic, k_, v_ )) 
+            print('*'*50)
 
 
 if __name__=='__main__':
@@ -23,7 +35,11 @@ if __name__=='__main__':
     MOZI_DF = pd.read_csv(FULL_MOZILLA_FILE) 
     
     merged_dataframe = mergeDataFrame(NVD_DF, MOZI_DF)
-    print(merged_dataframe.tail()) 
+    # print(merged_dataframe.tail()) 
+
+    ###RQ: What source code weaknesses are exploited with the identified tactics 
+    printMappedCWEs(merged_dataframe) 
+
     
 
 
