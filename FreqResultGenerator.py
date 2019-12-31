a10fa753f9987cc7baac0dd87fbfb2e821588332
Akond Rahman <akond.rahman.buet@gmail.com>
2019-12-31 11:39:16 -0600

    get frequency of tactics, proportion

--- FreqResultGenerator.py
+++ FreqResultGenerator.py
@@ -21,6 +21,17 @@ def getTacticProportion(full_df):
         print('ALL:{},TACTIC_NAME:{},TACTIC_COUNT:{},TACTIC_PROP:{}'.format(len(all_tactics), tact, len(tact_list), float(len(tact_list))/float(len(all_tactics))))
         print('-'*25) 
 
+
+def getCVEProportion(full_df):
+    all_tactics    = full_df['TACTIC'].tolist() 
+    unique_tactics = np.unique(all_tactics) 
+    unique_cves    = np.unique(full_df['CVE'].tolist())
+    for tact in unique_tactics:
+        tact_df       =  full_df[full_df['TACTIC']==tact]
+        cve_tact_list = tact_df['CVE'].tolist()
+        print('CVE_COUNT:{},TACTIC_NAME:{},CVE_PROP:{}'.format(len(unique_cves), tact, float(len(cve_tact_list))/float(len(unique_cves))))
+        print('-'*25) 
+
 if __name__=='__main__':
 
     ###NVD FILE
@@ -34,4 +45,7 @@ if __name__=='__main__':
     DATASET_DF  = pd.read_csv(DATASET_FILE) 
     merged_dataframe = mergeDataFrame(NVD_DF, DATASET_DF)
     # print(merged_dataframe.tail())   
+    ## Proportion of tactics 
     getTacticProportion(merged_dataframe)     
+    ## Proportion of CVEs
+    getCVEProportion(merged_dataframe)        
