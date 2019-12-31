8cbf4e9c6f79b6bc8da2f8ee5c44c3ae2afd0552
Akond Rahman <akond.rahman.buet@gmail.com>
2019-12-31 11:30:04 -0600

    get frequency of tactics, proportion

--- FreqResultGenerator.py
+++ FreqResultGenerator.py
@@ -4,7 +4,22 @@ Akond Rahman
 Dec 31, 2019 
 Tuesday 
 '''
+import pandas as pd 
+import numpy as np 
 
+def mergeDataFrame(nvd_df, mapping_df):
+    mapping_df['CVE'] =  mapping_df['CVE'].str.upper() 
+    # print(mapping_df.head())
+    merged_df  =  nvd_df.merge(mapping_df, left_on='ID', right_on='CVE')
+    return merged_df 
+
+def getTacticProportion(full_df):
+    all_tactics = full_df['TACTIC'].tolist() 
+    unique_tactics = np.unique(all_tactics) 
+    for tact in unique_tactics:
+        tact_list = [z for z in all_tactics if z==tact] 
+        print('ALL:{},TACTIC_NAME:{},TACTIC_COUNT:{},TACTIC_PROP:{}'.format(len(all_tactics), tact, len(tact_list), float(len(tact_list))/float(len(all_tactics))))
+        print('-'*25) 
 
 if __name__=='__main__':
 
@@ -18,4 +33,5 @@ if __name__=='__main__':
     NVD_DF = pd.read_csv(FULL_NVD_FILE)
     DATASET_DF  = pd.read_csv(DATASET_FILE) 
     merged_dataframe = mergeDataFrame(NVD_DF, DATASET_DF)
-    print(merged_dataframe.tail())        
+    # print(merged_dataframe.tail())   
+    getTacticProportion(merged_dataframe)     
