01b041d24b18189770b6edf28860d541479fd65a
Akond Rahman <akond.rahman.buet@gmail.com>
2019-12-31 12:32:41 -0600

    get frequency of tactics, cve

--- FreqResultGenerator.py
+++ FreqResultGenerator.py
@@ -10,8 +10,9 @@ import numpy as np
 def mergeDataFrame(nvd_df, mapping_df):
     mapping_df['CVE'] =  mapping_df['CVE'].str.upper() 
     # print(mapping_df.head())
-    merged_df  =  nvd_df.merge(mapping_df, left_on='ID', right_on='CVE')
-    return merged_df 
+    merged_df   =  nvd_df.merge(mapping_df, left_on='ID', right_on='CVE')
+    filtered_df =  merged_df[merged_df['SEVERITY']!='NOT_FOUND']
+    return filtered_df 
 
 def getTacticProportion(full_df):
     all_tactics = full_df['TACTIC'].tolist() 
@@ -28,24 +29,51 @@ def getCVEProportion(full_df):
     unique_cves    = np.unique(full_df['CVE'].tolist())
     for tact in unique_tactics:
         tact_df       =  full_df[full_df['TACTIC']==tact]
-        cve_tact_list = tact_df['CVE'].tolist()
+        cve_tact_list =  np.unique(tact_df['CVE'].tolist() )
         print('CVE_COUNT:{},TACTIC_NAME:{},CVE_PROP:{}'.format(len(unique_cves), tact, float(len(cve_tact_list))/float(len(unique_cves))))
         print('-'*25) 
 
 if __name__=='__main__':
+    '''
+    THE FOLLWOING WILL BE EXCLUDED FROM PAPER 
+    '''
+    # #LIBREOFFICE  
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-LIBREOFFICE-MAPPING-FINAL.csv'    
+    # #GENTOO   
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-GENTOO-MAPPING-FINAL.csv'
+    # #HTTPD     
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-HTTPD-MAPPING-FINAL.csv'
+    '''
+    '''
 
     ###NVD FILE
     FULL_NVD_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/RAW/FINAL_NVD_CVE_DATA_FULL.csv'
     ###TACTIC MAPPING FILE
-    # #MOZILLA 
-    DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-MOZILLA-MAPPING-FINAL.csv' 
+
+    # # #CHROME  
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-CHROME-MAPPING-FINAL.csv'
+    # #ECLIPSE  
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-ECLIPSE-MAPPING-FINAL.csv'
+    # #MOBY    
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-MOBY-MAPPING-FINAL.csv'
+    # # #MOZILLA 
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-MOZILLA-MAPPING-FINAL.csv' 
+    #OPENSTACK 
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-OPENSTACK-MAPPING-FINAL.csv'    
+    # #PHP 
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-PHP-MAPPING-FINAL.csv'    
+    # #REDHAT  
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-REDHAT-MAPPING-FINAL.csv'
 
 
     NVD_DF = pd.read_csv(FULL_NVD_FILE)
     DATASET_DF  = pd.read_csv(DATASET_FILE) 
     merged_dataframe = mergeDataFrame(NVD_DF, DATASET_DF)
     # print(merged_dataframe.tail())   
+    print('ANALYZING:', DATASET_FILE.split('/')[-1])
+    print('='*100)
     ## Proportion of tactics 
     getTacticProportion(merged_dataframe)     
+    print('='*100)
     ## Proportion of CVEs
     getCVEProportion(merged_dataframe)        
