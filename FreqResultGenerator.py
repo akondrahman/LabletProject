105b0de5799ffae14ee9d56c9bce51c0ff7cea98
Akond Rahman <akond.rahman.buet@gmail.com>
2019-12-31 19:16:10 -0600

    get frequency of subcateg

--- FreqResultGenerator.py
+++ FreqResultGenerator.py
@@ -6,6 +6,7 @@ Tuesday
 '''
 import pandas as pd 
 import numpy as np 
+from collections import Counter 
 
 def mergeDataFrame(nvd_df, mapping_df):
     mapping_df['CVE'] =  mapping_df['CVE'].str.upper() 
@@ -33,6 +34,48 @@ def getCVEProportion(full_df):
         print('CVE_COUNT:{},TACTIC_NAME:{},CVE_PROP:{}'.format(len(unique_cves), tact, float(len(cve_tact_list))/float(len(unique_cves))))
         print('-'*25) 
 
+
+def splitOnCateg(df_, name_, categ_):
+    subcateg_list = []
+    bug_content_file, bug_content_txt = '', ''
+    if 'MOZILLA' in name_:
+        bug_content_file = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/RAW/MOZILLA-SUBCATEGORY-MAPPING.csv'
+    categ_df = df_[df_['TACTIC']==categ_]
+    for bugID in np.unique( categ_df['BUGID'].tolist() ):
+        SUBCATEG = ''
+        bug_content_df   = pd.read_csv(bug_content_file) 
+        content_ls       = bug_content_df[bug_content_df['BUGID']==bugID]['CONTENT'].tolist()
+        if len(content_ls) > 0:
+            bug_content_txt  = content_ls[0].lower()
+        # print(bugID, bug_content_txt) 
+        if ('pdf' in bug_content_txt and 'file' in bug_content_txt) or ('.exe' in bug_content_txt): 
+            SUBCATEG = 'BINARY'
+        elif('cert' in bug_content_txt): 
+            SUBCATEG = 'CERTIFICATE' 
+        elif(('mp3' in bug_content_txt) or ('mp4' in bug_content_txt) or ('audio' in bug_content_txt) or ('video' in bug_content_txt)) : 
+            SUBCATEG = 'MULTIMEDIA'
+        else:
+            SUBCATEG = 'SOURCECODE'  
+        subcateg_list.append(SUBCATEG) 
+    return subcateg_list
+        
+
+
+
+def printSubCateg(ls, name_):
+    the_dict = dict(Counter(ls))
+    total_ = len(ls)
+    for k_, v_ in the_dict.items():
+        prop = round( float(v_)/float(total_) , 5)
+        print('CATEG:{}, SUB_CATEG:{}, PROP:{}'.format(name_, k_, prop))
+        print('*'*25)
+
+
+
+def printSubCategFreq(full_df, name_):
+    ls = splitOnCateg(full_df, name_, 'PAYLOAD')
+    printSubCateg(ls, 'PAYLOAD')
+
 if __name__=='__main__':
     '''
     THE FOLLWOING WILL BE EXCLUDED FROM PAPER 
@@ -43,6 +86,8 @@ if __name__=='__main__':
     # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-GENTOO-MAPPING-FINAL.csv'
     # #HTTPD     
     # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-HTTPD-MAPPING-FINAL.csv'
+    # #REDHAT  
+    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-REDHAT-MAPPING-FINAL.csv'
     '''
     '''
 
@@ -57,23 +102,25 @@ if __name__=='__main__':
     # #MOBY    
     # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-MOBY-MAPPING-FINAL.csv'
     # # #MOZILLA 
-    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-MOZILLA-MAPPING-FINAL.csv' 
+    DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-MOZILLA-MAPPING-FINAL.csv' 
     #OPENSTACK 
     # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-OPENSTACK-MAPPING-FINAL.csv'    
     # #PHP 
     # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-PHP-MAPPING-FINAL.csv'    
-    # #REDHAT  
-    # DATASET_FILE='/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/LOCKED_DATASETS/TACTIC-MAPPING/LOCKED-REDHAT-MAPPING-FINAL.csv'
 
 
-    NVD_DF = pd.read_csv(FULL_NVD_FILE)
-    DATASET_DF  = pd.read_csv(DATASET_FILE) 
+
+    NVD_DF       = pd.read_csv(FULL_NVD_FILE)
+    DATASET_DF   = pd.read_csv(DATASET_FILE) 
+    DATASET_NAME = DATASET_FILE.split('/')[-1]
     merged_dataframe = mergeDataFrame(NVD_DF, DATASET_DF)
     # print(merged_dataframe.tail())   
-    print('ANALYZING:', DATASET_FILE.split('/')[-1])
+    print('ANALYZING:', DATASET_DF)
     print('='*100)
     ## Proportion of tactics 
-    getTacticProportion(merged_dataframe)     
-    print('='*100)
-    ## Proportion of CVEs
-    getCVEProportion(merged_dataframe)        
+    # getTacticProportion(merged_dataframe)     
+    # print('='*100)
+    # ## Proportion of CVEs
+    # getCVEProportion(merged_dataframe) 
+    ## Get Sub category proportion 
+    printSubCategFreq(merged_dataframe, DATASET_NAME)        
