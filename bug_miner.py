6a0f06281c31f7d54c3dbec6e59498499cba14ed
Akond Rahman <akond.rahman.buet@gmail.com>
2020-01-25 02:05:51 -0600

    email mining

--- bug_miner.py
+++ bug_miner.py
@@ -7,8 +7,9 @@ import pandas as pd
 import numpy as np
 import json 
 import requests
-import cPickle as pickle 
-api_token = ''
+import _pickle  as pickle 
+# api_token = ''  ## for Eclispe 
+api_token = ''  ## for Mozilla  
 
 def processDict(dict_p):
     data_list = []
@@ -30,8 +31,6 @@ def processDict(dict_p):
            severity       = bug_['severity']           
            status         = bug_['status'] 
            summary        = bug_['summary']  
-           if 'cve' in summary:
-               print 'Summary:', summary 
            url            = ''
            if 'url' in bug_:
               url = bug_['url']
@@ -58,16 +57,16 @@ def getBugData(df_param):
     return all_bug_data
 
 def getSecBugData(lis_par):
-    print 'Need to process:', len(lis_par)
+    # print 'Need to process:', len(lis_par)
     all_bug_data = {}
     already = pickle.load(open('TMP_SEC_CMT_1.PKL', 'rb'))
-    print 'Already processed:', len(already)
+    # print 'Already processed:', len(already)
     for bugIDTuple  in lis_par:
         bugID, bugSeverity = bugIDTuple
         if bugID not in already:
             req_url  = 'https://bugzilla.mozilla.org/rest/bug/' + str(bugID).strip()    
             bug_data = requests.get(req_url, params={'api_key': api_token })  
-            print bugID , bugSeverity
+            # print bugID , bugSeverity
             # print bug_data         
             bug_dict = bug_data.json()
             # print bug_dict 
@@ -80,6 +79,50 @@ def getSecBugData(lis_par):
     return all_bug_data
 
 
+def getEclipseEmails(full_df, out_file):  
+    # reff: https://bugs.eclipse.org/bugs/rest/bug/376589
+    email_ls = []
+    bugIDs = np.unique( full_df['BUGID'].tolist() )
+    bugIDs = [x_ for x_ in bugIDs if  x_.isnumeric()   ] 
+    for bugID in bugIDs: 
+        bugDF    = full_df[full_df['BUGID']==bugID]
+        cve      = bugDF['CVE'].tolist()[0]
+        req_url  = 'https://bugs.eclipse.org/bugs/rest/bug/' + str(bugID).strip()    
+        bug_data = requests.get(req_url, params={'api_key': api_token })  
+        bug_dict = bug_data.json()
+        bug_reporter = 'NOT_FOUND' 
+        # print(bug_dict) 
+        if 'bugs' in bug_dict:
+            bug_details = bug_dict['bugs'] 
+            if 'creator' in bug_details[0]:
+                bug_reporter = bug_details[0]['creator']
+                print(bugID, cve,  bug_reporter)
+        email_ls.append( (bugID, cve,  bug_reporter) )
+    email_df = pd.DataFrame( email_ls )
+    email_df.to_csv(out_file, header=['BUGID', 'CVE' , 'EMAIL' ], index=False, encoding='utf-8')
+    
+
+def getMozillaEmails(full_df, out_file):  
+    email_ls = []
+    bugIDs = np.unique( full_df['BUGID'].tolist() )
+    for bugID in bugIDs: 
+        bugDF    = full_df[full_df['BUGID']==bugID]
+        cve      = bugDF['CVE'].tolist()[0]
+        req_url  = 'https://bugzilla.mozilla.org/rest/bug/' + str(bugID).strip()    
+        bug_data = requests.get(req_url, params={'api_key': api_token })  
+        bug_dict = bug_data.json()
+        bug_reporter = 'NOT_FOUND' 
+        # print(bug_dict) 
+        if 'bugs' in bug_dict:
+            bug_details = bug_dict['bugs'] 
+            if 'creator' in bug_details[0]:
+                bug_reporter = bug_details[0]['creator']
+                print(bugID, cve,  bug_reporter)
+        email_ls.append( (bugID, cve,  bug_reporter) )
+    email_df = pd.DataFrame( email_ls )
+    email_df.to_csv(out_file, header=['BUGID', 'CVE' , 'EMAIL' ], index=False, encoding='utf-8')
+
+
 if __name__=='__main__':
 #    bug_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/2018/2018.bug.crash.mapping.csv'
 #    bug_df = pd.read_csv(bug_file)
@@ -87,7 +130,21 @@ if __name__=='__main__':
 #    pickle.dump( all_bug_dict, open('/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/2018/2018.NEEDED.BUG.DETAILS.PKL', 'wb')) 
 #    print all_bug_dict  
 
-     sec_bug_pkl = '/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/SECU_BUG_IDS.PKL'
-     lis = pickle.load(open(sec_bug_pkl, 'rb')) 
-     sec_bug_dic = getSecBugData(lis)
-     pickle.dump( sec_bug_dic, open('/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/SEC_BUG_PROP.PKL', 'wb'))      
+    #  sec_bug_pkl = '/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/SECU_BUG_IDS.PKL'
+    #  lis = pickle.load(open(sec_bug_pkl, 'rb')) 
+    #  sec_bug_dic = getSecBugData(lis)
+    #  pickle.dump( sec_bug_dic, open('/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/SEC_BUG_PROP.PKL', 'wb'))      
+
+
+    '''
+    TO GET EMAILS 
+    '''
+    # BUG_ID_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/SurveyWork/SurveyEmailList/LOCKED-ECLIPSE-MAPPING-FINAL.csv'
+    # OUT_FILE    = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/SurveyWork/SurveyEmailList/ECLIPSE-EMAILS.csv'
+    # BUG_ID_DF   = pd.read_csv(BUG_ID_FILE) 
+    # getEclipseEmails(BUG_ID_DF, OUT_FILE)
+
+    # BUG_ID_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/SurveyWork/SurveyEmailList/LOCKED-MOZILLA-MAPPING-FINAL.csv'
+    # OUT_FILE    = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/SurveyWork/SurveyEmailList/MOZILLA-EMAILS.csv'
+    # BUG_ID_DF   = pd.read_csv(BUG_ID_FILE) 
+    # getMozillaEmails(BUG_ID_DF, OUT_FILE)
--- launchpad_bug_comments.py
+++ launchpad_bug_comments.py
@@ -12,7 +12,7 @@ launchpad = Launchpad.login_anonymously('just testing', 'production')
 import numpy as np 
 import os 
 import time 
-import cPickle as pickle 
+import _pickle  as pickle 
 import pandas as pd 
 import time 
 import  datetime 
@@ -35,7 +35,7 @@ def getCommentForBug(bugID):
     bug_date     = the_bug.date_created
     bug_date_str = bug_date.strftime('%Y-%m-%dT%H-%M-%S') ## date with time 
     if len(bug_comments) > 0 : 
-        print 'Analyzing {} comments for bug#{}'.format(len(bug_comments), bugID)
+        # print 'Analyzing {} comments for bug#{}'.format(len(bug_comments), bugID)
         # print bugID 
         for comment_obj in bug_comments:
                 str1_ = 'BUG_ID:' + str(bugID) + '\n' + '-'*10 + '\n'
@@ -53,9 +53,6 @@ def getCommentForBug(bugID):
                     bugTupleList.append( (bugID, bug_cve, comment_index, comment_subject, comment_owner, bug_date_str, com_date_str) )
                     csvStr = csvStr + str(bugID) + ',' + bug_cve +  ',' + str(comment_index) + ',' + comment_subject + ',' + str(comment_owner ) + ',' + bug_date_str + ',' + com_date_str   + '\n'
                     comment_index += 1 
-                
-    else: 
-        print 'No comments found  for bug#', bugID 
     # print bugStr
     return bugTupleList, csvStr
 
@@ -73,8 +70,8 @@ def getBugComments(file_name, out_file, out_csv_file, pkl_out_file):
          lines = f_.readlines()
     ID_list = [int(x_) for x_ in lines if x_!= '\n']
     unique_IDs = np.unique(ID_list) 
-    print 'Total bugs to analyze:', len( unique_IDs )
-    print '*'*50 
+    # print 'Total bugs to analyze:', len( unique_IDs )
+    # print '*'*50 
     for bugID in unique_IDs: 
             bug_details, csv_str  = getCommentForBug(bugID)
             complete_csv_str = complete_csv_str + csv_str 
@@ -84,6 +81,28 @@ def getBugComments(file_name, out_file, out_csv_file, pkl_out_file):
 
     dumpContentIntoFile(complete_csv_str, out_csv_file)  
     
+
+def getOpenstackEmails(df_, out_file):
+    emailList = []
+    bugIDs = np.unique( df_['BUGID'].tolist() )
+    for bugID in bugIDs:
+        bugDF          = df_[df_['BUGID']==bugID] 
+        cve            = bugDF['CVE'].tolist()[0] 
+        the_bug        = launchpad.bugs[bugID]  
+        the_owner      = the_bug.owner
+        owner_name     = the_owner.name 
+        owner_emails   = the_owner.preferred_email_address_link
+        owner_link     = 'https://launchpad.net/~' + owner_name 
+        tuple_         = (bugID, cve, owner_name, owner_emails, owner_link)
+        print(tuple_)   
+        emailList.append( tuple_ )
+    email_df = pd.DataFrame( emailList )
+    email_df.to_csv(out_file, header=['BUGID', 'CVE' , 'NAME', 'EMAIL', 'URL' ], index=False, encoding='utf-8')
+
+
+
+
+
 
 if __name__=='__main__':
     bugID_file          = '/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/dataset-bugreport-synthesis/ost-with-cves-only/OST_BUG_IDS_RES.txt' 
@@ -97,4 +116,13 @@ if __name__=='__main__':
     # output_csv_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/UBUNTU_CVE_BUG_REPORT_COMMENT_MAP.csv'
     # output_pkl_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SoSLablet/Fall-2018/datasets/UBUNTU_CVE_BUG_REPORT_COMMENT_ALL.PKL'
 
-    getBugComments(bugID_file, output_comment_file, output_csv_file, output_pkl_file)  
+    # getBugComments(bugID_file, output_comment_file, output_csv_file, output_pkl_file)  
+
+    '''
+    TO GET EMAILS 
+    '''
+    # BUG_ID_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/SurveyWork/SurveyEmailList/LOCKED-OPENSTACK-MAPPING-FINAL.csv'
+    # OUT_FILE    = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/SurveyWork/SurveyEmailList/OPENSTACK-EMAILS.csv'
+    # BUG_ID_DF   = pd.read_csv(BUG_ID_FILE) 
+    # getOpenstackEmails(BUG_ID_DF, OUT_FILE)
+ 
